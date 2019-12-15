53d49fa81a4f2e5bf69c373df5abefe8fb23ecf6
chaliburton <chris.haliburton@gmail.com>
2019-12-15 16:37:33 +0000

    Initial commit

---
+++ set_git.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Make sure you have the latest version of the repo
+echo
+git pull
+echo
+
+# Ask the user for login details
+read -p 'https://github.com/chaliburton/CarND-Kidnapped-Vehicle-Project-Particle-Filter.git' upstreamVar
+read -p 'chaliburton' userVar
+read -p 'chris.haliburton@gmail.com' emailVar
+
+echo
+echo Thank you $userVar!, we now have your credentials
+echo for upstream $upstreamVar. You must supply your password for each push.
+echo
+
+echo setting up git
+
+git config --global user.name $userVar
+git config --global user.email $emailVar
+git remote set-url origin $upstreamVar
+echo
+
+echo Please verify remote:
+git remote -v
+echo
+
+echo Please verify your credentials:
+echo username: `git config user.name`
+echo email: `git config user.email`
